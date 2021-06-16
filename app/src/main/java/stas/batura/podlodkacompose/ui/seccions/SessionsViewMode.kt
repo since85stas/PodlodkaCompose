00bf76d0a4335fcd791d2108d90b6e9db6f804be
home <seeyou85@gmail.com>
2021-06-16 12:37:08 +0400

    проблемы с зависимостями

--- app/src/main/java/stas/batura/podlodkacompose/ui/seccions/SessionsViewMode.kt
+++ app/src/main/java/stas/batura/podlodkacompose/ui/seccions/SessionsViewMode.kt
@@ -3,30 +3,19 @@ package stas.batura.podlodkacompose.ui.seccions
 import android.util.Log
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.*
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import stas.batura.podlodkacompose.data.IRepository
-import stas.batura.podlodkacompose.di.ApplicationScope
-import javax.inject.Inject
 
 private val TAG = "PayViewModel"
 
-class SessionsViewModel @ViewModelInject constructor(
-    val repository: IRepository,
-    @ApplicationScope
-    val externalScope: CoroutineScope
-    ): ViewModel() {
+class SessionsViewModel @ViewModelInject constructor(val repository: IRepository): ViewModel() {
 
     private val _toastText = MutableLiveData<String>()
     val toastTex: LiveData<String> get() = _toastText
 
     private val _spinner = MutableLiveData<Boolean>(false)
     val spinner: LiveData<Boolean> get() = _spinner
-
-//    @Inject
-//    @ApplicationScope
-//    lateinit var externalScope: CoroutineScope
 
 
     init {
@@ -36,20 +25,19 @@ class SessionsViewModel @ViewModelInject constructor(
 
     private fun loadData() {
         launchDataLoad {
-            repository.addInitsessions()
         }
     }
 
     private fun launchDataLoad(block: suspend () -> Unit): Job {
-        return externalScope.launch {
+        return viewModelScope.launch {
             try {
-                _spinner.postValue(true)
+                _spinner.value = true
                 block()
             } catch (error: Throwable) {
                 Log.d(TAG, "launchDataLoad: " + error)
                 _toastText.value = "Internet problem"
             } finally {
-                _spinner.postValue(false)
+                _spinner.value = false
             }
         }
     }
